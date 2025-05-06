f90a77198eefc1d47c58455914046bf0754c5ab5
ersic <ersic@duck.com>
2025-05-06 16:46:18 +0800

    Add Jieba PHP library and new command for word segmentation
    Updated composer.json and composer.lock to include fukuball/jieba-php library. Introduced NodeDailyJieba command for processing post titles with Jieba word segmentation. Added user dictionary file for custom tokenization.

---
+++ app/command/NodeDailyJieba.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace app\command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+use app\model\Post;
+use Fukuball\Jieba\Jieba;
+use Fukuball\Jieba\Finalseg;
+
+class NodeDailyJieba extends Command
+{
+    protected static $defaultName = 'nodeDaily:jieba';
+    protected static $defaultDescription = 'nodeDaily jieba';
+
+    /**
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->addArgument('name', InputArgument::OPTIONAL, 'Name description');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $name = $input->getArgument('name');
+        $output->writeln('Starting Jieba word segmentation for post titles...');
+        
+        ini_set('memory_limit', '1024M');
+        // Initialize Jieba
+        Jieba::init();
+        Finalseg::init();
+        Jieba::loadUserDict(base_path() . '/app/command/user_dict.txt');
+
+        // Get posts that haven't been tokenized yet
+        $posts = Post::where('is_token', 0)->where('id', 332015)->limit(10)->get();
+        
+        foreach ($posts as $post) {
+            // Perform word segmentation on the title
+            $words = Jieba::cut($post->title);
+            
+            // Join the words with spaces and save
+            // $post->title_tokens = implode(' ', $words);
+            // $post->is_token = 1;
+            // $post->save();
+            
+            $output->writeln("Processed post ID: {$post->id}, Title: {$post->title}");
+            $output->writeln($words);
+        }
+
+        $output->writeln('Word segmentation completed!');
+        return self::SUCCESS;
+    }
+}
